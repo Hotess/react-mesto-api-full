c4be39ffd5a205da41629622154a10e906272b97
Pavel Opolovnin <hote_95@bk.ru>
2021-02-04 08:21:01 +0300

    complected

--- backend/app.js
+++ backend/app.js
@@ -20,9 +20,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-  ],
+  origin: '*',
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
