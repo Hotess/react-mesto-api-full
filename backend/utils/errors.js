93cde458601d944e9c67aae8a247a7762e58b2ac
Pavel Opolovnin <hote_95@bk.ru>
2021-02-05 02:59:19 +0300

    all

--- backend/utils/errors.js
+++ backend/utils/errors.js
@@ -5,14 +5,14 @@ const Conflict = require('../errors/Conflict');
 
 module.exports.currentError = (err, res) => {
   if (err.name === 'CastError' || err.name === 'ValidationError') {
-    throw new BadRequestError({ message: `Указаны некорректные данные: ${err.message}` });
+    throw new BadRequestError({ message: 'Указаны некорректные данные' });
   } else if (err.name === 'DocumentNotFoundError') {
-    throw new NotFoundError({ message: `Объект не найден: ${err.message}` });
+    throw new NotFoundError({ message: 'Объект не найден' });
   } else if (err.name === 'ForbiddenError') {
-    throw new ForbiddenError({ message: `Недостаточно прав для выполнения операции: ${err.message}` });
+    throw new ForbiddenError({ message: 'Недостаточно прав для выполнения операции' });
   } else if (err.name === 'Conflict') {
-    throw new Conflict({ message: `Такой аккаунт уже существует: ${err.message}` });
+    throw new Conflict({ message: 'Такой аккаунт уже существует' });
   } else {
-    res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+    res.status(500).send({ message: 'Ошибка сервера' });
   }
 };
